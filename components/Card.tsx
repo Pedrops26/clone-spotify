17ec257b574ddcd4fd0a3f6f06bc8480a83112b3
Pedrops26 <pedro.saldarriaga@udea.edu.co>
2023-10-02 09:05:07 -0500

    ..

--- components/Card.tsx
+++ components/Card.tsx
@@ -11,7 +11,9 @@ const Card = ({title, description, buttonText}: CardProps) => {
         <div className="flex flex-col bg-light-gray items-start p-4 rounded-lg gap-3">
             <span className="font-semibold text-lg">{title}</span>
             <span className="text-md">{description}</span>
-            <Button text={buttonText} type='secondary' />
+            <Button text={buttonText} type='secondary' handleClick={()=>{
+                            (true);
+                    }}/>
         </div>
     )
 }
